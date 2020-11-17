4444409b2af9cb626cc963a9cebb5ed1e3370495
GremmLeN <GremmLeN@gmail.com>
2020-11-17 22:30:41 +0500

    homework-lesson4

---
+++ geek-java-EE/jsf-web-app/src/main/java/servlet/OrderBean.java
@@ -0,0 +1,39 @@
+package servlet;
+
+import entity.Product;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.inject.Inject;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+
+@ManagedBean(name = "order")
+@SessionScoped
+public class OrderBean {
+    private Logger logger = LoggerFactory.getLogger(OrderBean.class);
+    private List<Product> orders = new LinkedList<>();
+
+    @Inject
+    private Cart cart;
+
+    public Collection<Product> getProductList() {
+        return cart.getAll();
+    }
+
+    public String doOrder(Product product) {
+        cart.add(product);
+        logger.info("Ordered {}", product.getName());
+        return "/index.xhtml?faces-redirect=true";
+    }
+
+    public String doDelete(Product product) {
+        cart.delete(product);
+        logger.info("Deleted {}", product.getName());
+        return "/orders.xhtml?faces-redirect=true";
+    }
+}
